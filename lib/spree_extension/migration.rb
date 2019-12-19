561158dde6e1e98c4c5c44de3a291e6d453118fe
Greg Benedict <gbenedict@tgfi.net>
2019-12-19 15:12:42 -0500

    Fixed lookup for Rails version number

--- lib/spree_extension/migration.rb
+++ lib/spree_extension/migration.rb
@@ -3,7 +3,7 @@ module SpreeExtension
     def self.[](version)
       # Rails 5.1 requires to use versioned migrations
       # but < 5.0 doesn't support them
-      if Rails.gem_version >= Gem::Version.new('5.x')
+      if Rails.respond_to?(:gem_version) && Rails.gem_version >= Gem::Version.new('5.x')
         ActiveRecord::Migration[version]
       else
         ActiveRecord::Migration
